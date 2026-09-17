1aca8d2a91a76dc9bff2ed29e37e0d2fa004d5dc
numidia.js <benamarnumidia03@gmail.com>
2026-09-17 14:47:57 +0200

    first commit

---
+++ ex1.js
@@ -0,0 +1,2 @@
+let age = 23;
+console.log(`your are ${age} yours old.`);
---
+++ exr2.js
@@ -0,0 +1,7 @@
+let temperature = 37;
+if ( temperature > 30) {
+    console.log ("It's hot outside!");
+}else{
+    console.log("The weather is nice");
+}
+   
